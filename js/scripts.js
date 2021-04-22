448fd810c4e4db1d3e48429a3203cae3e435d6bd
Daniel Brett <drummer.man.dan@icloud.com>
2021-04-22 16:32:32 -0500

    Minimum Viable Front-End

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#staffLogin").click(function() {
+        $("#staffLoginModal").modal("show");
+    });
+    $("#studentLogin").click(function() {
+        $("#studentLoginModal").modal("show");
+    });
+});
